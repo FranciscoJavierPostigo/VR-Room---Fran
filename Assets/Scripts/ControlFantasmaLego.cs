7064175ce45fcf560e53fdf23116962e943bd581
FranciscoJavierPostigo <91095508+FranciscoJavierPostigo@users.noreply.github.com>
2026-05-31 00:17:55 +0200

    Update ControlFantasmaLego.cs

--- Assets/Scripts/ControlFantasmaLego.cs
+++ Assets/Scripts/ControlFantasmaLego.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 using UnityEngine.XR.Interaction.Toolkit.Interactables; 
+
 public class ControlFantasmaLego : MonoBehaviour
 {
-    [Header("Arrastra aquí el fantasma de esta pieza")]
+    [Header("Andamiaje Visual")]
     public GameObject miFantasma;
 
     private XRGrabInteractable grabInteractable;
@@ -13,27 +14,26 @@ public class ControlFantasmaLego : MonoBehaviour
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
 
-        // Nos aseguramos de que empiece apagado por si se nos olvida en el editor
-        if (miFantasma != null) miFantasma.SetActive(false);
+        if (miFantasma != null) 
+            miFantasma.SetActive(false);
 
-        // Le decimos al código que escuche automáticamente cuando agarramos y soltamos la pieza
         grabInteractable.selectEntered.AddListener(AlAgarrar);
         grabInteractable.selectExited.AddListener(AlSoltar);
     }
 
     private void AlAgarrar(SelectEnterEventArgs args)
     {
-        // MAGIA: Solo encendemos el fantasma si lo agarra el jugador (una mano).
-        // Si lo "agarra" el Socket al encajarlo, lo ignoramos para que no vuelva a aparecer.
+        // Evitamos reactivar la malla translÃºcida si el evento Select es disparado por un encaje automÃ¡tico (Socket)
         if (!(args.interactorObject is XRSocketInteractor))
         {
-            if (miFantasma != null) miFantasma.SetActive(true);
+            if (miFantasma != null) 
+                miFantasma.SetActive(true);
         }
     }
 
     private void AlSoltar(SelectExitEventArgs args)
     {
-        // Al soltarlo en la mesa o al encajarlo en el socket, el fantasma se apaga siempre.
-        if (miFantasma != null) miFantasma.SetActive(false);
+        if (miFantasma != null) 
+            miFantasma.SetActive(false);
     }
 }
